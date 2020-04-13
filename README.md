f5ebaa7a5c0dff232309565757a55e3c60bb85db
Dave Smith <dave.k.smith@gmail.com>
2020-04-13 12:16:58 -0500

    Document version that's minimally installable by pip

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # tuulbachs
 
-A bachs of generally useful tuuls.
+## Usage
+
+1. Set up and activate a Python virtual environment[1] at the top level of this project.
+1. `pip install .`
+
+[1]: https://docs.python.org/3/tutorial/venv.html
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+# trick to install package needed by setup.py itself found here:
+#    https://stackoverflow.com/a/49817738
+import pip
+pip.main(['install', 'PyYAML'])
+
 from distutils.core import setup
 from version        import emit_version
 
 setup(
   name='tuulbachs',
   version=emit_version(),
-  packages=['python',],
   long_description=open('README.md').read(),
 )
