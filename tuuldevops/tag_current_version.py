2526f9da5f0a0dd0c72c5497e2d822b603ee1ac6
Dave Smith <dave.k.smith@gmail.com>
2020-04-25 17:47:54 -0500

    Move to signed tagging in tuuldevops/tag_current_version.py

--- tuuldevops/tag_current_version.py
+++ tuuldevops/tag_current_version.py
@@ -5,7 +5,7 @@ Git tag the commit on the current branch with the current version of this
 software
 """
 
-from tuulgit.tag_commit  import tag_current
+from tuulgit.tag_commit  import tag_current_signed
 from tuulver.version     import emit_version
 
 
@@ -15,7 +15,7 @@ def tag_product_version(conf_filename):
   Git tag the commit on the current branch with the version of this software
   given in conf_filename
   """
-  tag_current(emit_version(conf_filename))
+  tag_current_signed(emit_version(conf_filename))
 
 
 # -----
