b18f395fe426ad76c668a8e1e08e087f0dff8c8d
Dave Smith <dave.k.smith@gmail.com>
2020-05-16 16:43:59 -0500

    Strengthen the check_clean module

--- tuulgit/check_clean.py
+++ tuulgit/check_clean.py
@@ -1,10 +1,72 @@
 """
-check_clean.py
+check_status.py
 ===============================
-Check the Git working tree
+Check the status of the local git working tree
 """
 
-from tuulpy.subprocess import subpResult
+# Guidance:  https://stackoverflow.com/a/2659808
+
+from   io import StringIO
+import sh
+from   tuulbachs.exception import TuulError
+
+
+def is_working_tree():
+  """
+  Check if this is a git working tree at all
+  """
+  try:
+    sh.git.status()
+  except sh.ErrorReturnCode_128:
+    raise TuulError('There is no git working tree here')
+
+
+def repo_toplevel_path():
+  """
+  Return a string containing the path of the repo's top-level directory
+  """
+  buf = StringIO()
+  # Guide:  https://stackoverflow.com/a/957978
+  sh.git('rev-parse', '--show-toplevel', _out=buf)
+  return buf.getvalue().rstrip()
+
+
+def has_staged_uncommitted():
+  """
+  Return a boolean indicating whether the repository has staged, but
+  uncommitted changes
+  """
+  try:
+    sh.git('diff-index', '--quiet', '--cached', 'HEAD', '--')
+    return False
+  except sh.ErrorReturnCode_1:
+    return True
+
+
+def has_unstaged_changes():
+  """
+  Return a boolean indicating whether the working tree has unstaged changes
+  """
+  try:
+    sh.git('diff-index', '--quiet')
+    return False
+  except sh.ErrorReturnCode_1:
+    return True
+
+
+def has_untracked_unignored_files():
+  """
+  Return a boolean indicating whether the working tree has untracked, unignored
+  files
+  """
+  top = repo_toplevel_path()
+  try:
+    # Guide: https://stackoverflow.com/a42884225
+    sh.grep(sh.git('ls-files', top, '--exclude-standard', '--others'),
+            '-q', '^')
+    return True
+  except sh.ErrorReturnCode_1:
+    return False
 
 
 def is_clean_working_tree():
@@ -12,7 +74,7 @@ def is_clean_working_tree():
   Return a boolean indicating whether the Git working tree is clean or not
   """
   dirty = subpResult.run('git status --porcelain')
-  return not dirty  
+  return not dirty
 
 
 def main():
