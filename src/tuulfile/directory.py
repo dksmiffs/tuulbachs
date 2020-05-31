5db7386d4b6d185fca0ee32629de58092b10dc50
Dave Smith <dave.k.smith@gmail.com>
2020-05-31 13:47:07 -0500

    GH-6 Add cd feature to tuulfile.directory module

--- src/tuulfile/directory.py
+++ src/tuulfile/directory.py
@@ -3,8 +3,25 @@ directory.py
 ======================
 Provide useful directory manipulation functions
 """
-from os import mkdir
+from os import chdir, getcwd, mkdir, path
 from random import randint
+
+
+# -----
+class cd:
+    """
+    Context manager to change the working directory, from the following source:
+       https://stackoverflow.com/a/13197763
+    """
+    def __init__(self, newPath):
+        self.newPath = path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = getcwd()
+        chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        chdir(self.savedPath)
 
 
 # -----
--- tests/tuulfile/directory_test.py
+++ tests/tuulfile/directory_test.py
@@ -1,13 +1,26 @@
 """pytest unit tests"""
 
-from os import path
+from pathlib import Path
 from shutil import rmtree
-from tuulfile.directory import create_tmp_dir
+from tuulfile.directory import cd, create_tmp_dir
+
+
+def test_cd():
+    old = Path.cwd()
+    with cd('/tmp'):
+        assert Path.cwd != old
+        p = Path('./hellew.txt')
+        p.touch()
+        assert p.exists()
+        p.unlink()
+        assert not p.exists()
+    assert Path.cwd() == old
 
 
 def test_create_tmp_dir():
     """unit test create_tmp_dir"""
     tmp = create_tmp_dir()
-    assert path.exists(tmp) 
+    p = Path(tmp)
+    assert p.exists()
     rmtree(tmp)
-    assert not path.exists(tmp)
+    assert not p.exists()
